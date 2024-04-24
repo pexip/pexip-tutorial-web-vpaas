f4276dcc04cf85020fd1805deda6b385d682a532
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-04-24 12:40:46 +0200

    Step.01-Solution-Server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,55 @@
-// TODO (01) Import type RequestHandler from express
-import express from 'express'
+import express, { type RequestHandler } from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import fs from 'fs'
 import https from 'https'
 import config from 'config'
-// TODO (02) Import uuid and jsonwebtoken
-// TODO (03) Import createApi and withToken from '@pexip/vpaas-api'
+import { v4 as uuidv4 } from 'uuid'
+import jwt from 'jsonwebtoken'
+import { createApi, withToken } from '@pexip/vpaas-api'
 
 const port: string = config.get('server.port')
 
 const app = express()
 app.use(helmet())
 
-// TODO (02) Define function to generate a JWT token
+const createJwt = (): string => {
+  const apiAddress: string = config.get('vpaas.apiAddress')
+  const authEndpoint = `${apiAddress}/oauth/token`
+  const clientId = config.get('vpaas.credentials.clientId')
+  const privateKey = fs.readFileSync(
+    config.get('vpaas.credentials.privateKeyPath')
+  )
 
-// TODO (03) Create the api object using withToken
+  const scope = [
+    'meeting:create',
+    'meeting:read',
+    'meeting:write',
+    'participant:create',
+    'participant:read',
+    'participant:write'
+  ]
+  const requestId = uuidv4()
+
+  const token = jwt.sign(
+    {
+      iss: clientId, // Application Client UUID
+      sub: clientId, // Application Client UUID
+      aud: authEndpoint,
+      scope: scope.join(' ')
+    },
+    privateKey,
+    {
+      algorithm: 'RS384',
+      expiresIn: '60s',
+      jwtid: requestId
+    }
+  )
+
+  return token
+}
+
+const api = withToken(createJwt, config.get('vpaas.apiAddress'))(createApi())
 
 app.use(
   cors({
@@ -23,11 +57,35 @@ app.use(
   })
 )
 
-// TODO (05) Add a new route to get the api address
+app.get('/api-address', (async (req, res) => {
+  res.send(config.get('vpaas.apiAddress'))
+}) as RequestHandler)
 
-// TODO (06) Add a new route to create the meeting ID
+app.post('/meetings', (async (req, res) => {
+  try {
+    const response = await api.create()
+    if (response.status === 200) {
+      return res.json(response.data)
+    } else {
+      return res.status(500).send('Cannot create the meeting')
+    }
+  } catch (error) {
+    return res.status(500).send('Cannot create the meeting')
+  }
+}) as RequestHandler)
 
-// TODO (07) Add a new route to create the participant
+app.post('/meetings/:meetingId/participants', (async (req, res) => {
+  try {
+    const response = await api.participants({ meetingId: req.params.meetingId })
+    if (response.status === 200) {
+      return res.json(response.data)
+    } else {
+      return res.status(500).send(`Cannot get participants from the meeting`)
+    }
+  } catch (error) {
+    return res.status(500).send(`Cannot get participants from the meeting`)
+  }
+}) as RequestHandler)
 
 const options = {
   key: fs.readFileSync('dev-certs/key.pem'),
