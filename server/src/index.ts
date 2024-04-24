8c6f4a1433c8aa5b708c919b38d4994d435a6eac
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-04-24 12:40:16 +0200

    Step.01-Exercise-Server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,33 @@
+// TODO (01) Import type RequestHandler from express
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import fs from 'fs'
 import https from 'https'
 import config from 'config'
+// TODO (02) Import uuid and jsonwebtoken
+// TODO (03) Import createApi and withToken from '@pexip/vpaas-api'
 
 const port: string = config.get('server.port')
 
 const app = express()
 app.use(helmet())
 
+// TODO (02) Define function to generate a JWT token
+
+// TODO (03) Create the api object using withToken
+
 app.use(
   cors({
     origin: 'https://localhost:4000'
   })
 )
+
+// TODO (05) Add a new route to get the api address
+
+// TODO (06) Add a new route to create the meeting ID
+
+// TODO (07) Add a new route to create the participant
 
 const options = {
   key: fs.readFileSync('dev-certs/key.pem'),
