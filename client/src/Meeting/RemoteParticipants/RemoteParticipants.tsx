3c367d783066065332cec688c2a0ea718743c1d0
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-04-25 17:22:07 +0200

    Step.03-Exercise-Join-a-meeting

--- client/src/Meeting/RemoteParticipants/RemoteParticipants.tsx
+++ client/src/Meeting/RemoteParticipants/RemoteParticipants.tsx
@@ -1,7 +1,22 @@
 import { Grid } from '@pexip/components'
+// TODO (09) Import dependency StreamInfo
+// TODO (10) Import dependency TransceiverConfig
+// TODO (11) Import dependency Participant
 
 import './RemoteParticipants.css'
 
+// TODO (12) Define the RemoteParticipants props interface
+
+// TODO (13) Define the component props
 export const RemoteParticipants = (): JSX.Element => {
+  // TODO (14) Deconstruct the props
+
+  // TODO (15) Define the function getMediaStreams
+
+  // TODO (16) Calculate the number of remote participants and md
+
+  // TODO (17) Create an array of components for participants
+
+  // TODO (18) Return the participants in a Grid component
   return <Grid className="RemoteParticipants"></Grid>
 }
