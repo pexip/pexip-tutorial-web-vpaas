6e2a02b1c7a3e631ae9fa77f11c6d2a1ee3d0222
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-05-10 14:05:06 +0200

    Step.06-Exercise-Simulcast

--- client/src/Meeting/Settings/Settings.tsx
+++ client/src/Meeting/Settings/Settings.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
+// TODO (07): Import Select component from the @pexip/components package
 import { Bar, Button, Modal } from '@pexip/components'
 import { DevicesSelection, Selfview } from '@pexip/media-components'
 import { type MediaDeviceInfoLike } from '@pexip/media-control'
 import { LocalStorageKey } from '../../types/LocalStorageKey'
 import { filterMediaDevices } from '../../filter-media-devices'
+// TODO (08): Import RTPStreamId from the './RTPStreamId' file
 
 import './Settings.css'
 
@@ -21,6 +23,8 @@ export const Settings = (props: SettingsProps): JSX.Element => {
   const [videoInput, setVideoInput] = useState<MediaDeviceInfoLike>()
   const [audioInput, setAudioInput] = useState<MediaDeviceInfoLike>()
   const [audioOutput, setAudioOutput] = useState<MediaDeviceInfoLike>()
+
+  // TODO (09) Define a new state variable named incomingVideoQuality of type RTPStreamId
 
   const handleCancel = (): void => {
     localStream?.getTracks().forEach((track) => {
@@ -42,6 +46,7 @@ export const Settings = (props: SettingsProps): JSX.Element => {
       LocalStorageKey.AudioOutputKey,
       audioOutput?.deviceId ?? ''
     )
+    // TODO (10) Save the incomingVideoQuality state variable to the local storage
     localStream?.getTracks().forEach((track) => {
       track.stop()
     })
@@ -71,6 +76,9 @@ export const Settings = (props: SettingsProps): JSX.Element => {
       requestVideo().catch((e) => {
         console.error(e)
       })
+
+      // TODO (11) Load the incoming video quality from the local storage
+      // TODO (12) Set the incomingVideoQuality state variable to the loaded value
     }
   }, [props.isOpen])
 
@@ -106,6 +114,8 @@ export const Settings = (props: SettingsProps): JSX.Element => {
         onAudioOutputChange={setAudioOutput}
         setShowHelpVideo={() => {}}
       />
+
+      {/* TODO (13) Add a Select component to select the incoming video quality */}
 
       <Bar className="ButtonBar">
         <Button onClick={handleCancel} modifier="fullWidth" variant="tertiary">
--- client/src/types/LocalStorageKey.ts
+++ client/src/types/LocalStorageKey.ts
@@ -4,4 +4,5 @@ export enum LocalStorageKey {
   VideoInputKey = `${prefix}:videoInput`,
   AudioInputKey = `${prefix}:audioInput`,
   AudioOutputKey = `${prefix}:audioOutput`
+  // TODO (06) Add a new key named IncomingVideoQualityKey
 }
