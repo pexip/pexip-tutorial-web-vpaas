ea7a5494846e5471fa6a8dbde313fa3c0ead669b
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-05-10 14:07:43 +0200

    Step.06-Solution-Simulcast

--- client/src/types/StreamInfo.ts
+++ client/src/types/StreamInfo.ts
@@ -1,11 +1,11 @@
-// TODO (02) Import Layer type from the '@pexip/vpaas-api' package
-// TODO (03) Import RTPStreamId type from the './RTPStreamId' file
+import { type Layer } from '@pexip/vpaas-api'
+import { type RTPStreamId } from './RTPStreamId'
 
 export interface StreamInfo {
   streamId: string
   type: 'audio' | 'video'
   participantId: string
-  // TODO (04) Add a new property named layers of type Layer[]
+  layers: Layer[]
   mid?: string
-  // TODO (05) Add a new property named rid of type RTPStreamId
+  rid?: RTPStreamId
 }
